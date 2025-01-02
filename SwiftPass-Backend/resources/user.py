5137335df87d52f580b87b5d44150fbdb3cca2b6
gabkoi21 <12345678+username@users.noreply.github.com>
2025-01-02 22:31:07 +0000

    Add the user regestration endpoint and the deleting of user base on thier id

--- SwiftPass-Backend/resources/user.py
+++ SwiftPass-Backend/resources/user.py
@@ -2,8 +2,10 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
 
+
 from db import db
 from schemas import UserSchema
+from schemas import UserUpdateSchema
 from models import UserModel
 
 blp = Blueprint("users", __name__, description="Operations on users")
@@ -24,3 +26,30 @@ class UserRegister(MethodView):
         db.session.commit()
 
         return {"message": "User created successfully."}, 201
+    
+    
+    
+@blp.route("/user/<int:user_id>")
+class user(MethodView):
+    @blp.response(200, UserSchema)
+    def get(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        return user
+    
+    
+    @blp.arguments(UserUpdateSchema)
+    @blp.response(200, UserSchema)
+    def put(self, user_data, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        user.email = user_data["email"]
+        user.fullname = user_data["fullname"]
+        db.session.commit() 
+        return user
+    
+    @blp.response(204)
+    def delete(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        db.session.delete(user)
+        db.session.commit()
+        
+        return {"message": "User deleted successfully."}, 201
--- SwiftPass-Backend/schemas.py
+++ SwiftPass-Backend/schemas.py
@@ -1,10 +1,16 @@
 from marshmallow import Schema, fields
 import marshmallow as ma
 
-    
 class UserSchema(Schema):
     id = fields.Int(dump_only=True)
     email = fields.Str(required=True)
     fullname = fields.Str(required=True)
-    password = fields.Str(required=True, load_only=True)
-    timestamp = ma.fields.DateTime(required=True) 
+    password = fields.Str(required=True)
+    timestamp = ma.fields.DateTime()
+
+class UserUpdateSchema(Schema):
+    email = fields.Str()
+    fullname = fields.Str()
+    
+    
+    
